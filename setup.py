052758df88eb872568e8e3b761b8c233e35c5325
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2022-04-14 11:16:56 +0200

    pkg: build the cache ui in elm at install time
    And make sure we ship everything pre-compiled at distribution time.

--- setup.py
+++ setup.py
@@ -1,4 +1,28 @@
+from pathlib import Path
+import subprocess
+
 from setuptools import setup
+from setuptools.command.build_ext import build_ext
+
+
+WORKING_DIR = Path(__file__).resolve().parent
+STATIC_DIR = WORKING_DIR / 'tshistory_refinery' / 'refinery_static'
+
+
+def compile_elm(edit_kind, src):
+    """Compile elm component to JS"""
+    src = WORKING_DIR / 'elm' / src
+    out = STATIC_DIR / f'{edit_kind}_elm.js'
+    cmd = f'elm make --optimize --output {out} {src}'
+    subprocess.call(cmd, shell=True)
+
+
+class ElmBuild(build_ext):
+    """Build Elm components"""
+
+    def run(self):
+        compile_elm('cache', 'Cache.elm')
+        super().run()
 
 
 setup(name='tshistory_refinery',
@@ -6,6 +30,7 @@ setup(name='tshistory_refinery',
       author='Pythonian',
       author_email='aurelien.campeas@pythonian.fr, arnaud.campeas@pythonian.fr',
       packages=['tshistory_refinery'],
+      zip_safe=False,
       install_requires=[
           'tshistory',
           'tsview',
@@ -23,6 +48,10 @@ setup(name='tshistory_refinery',
           'pygments',
           'croniter'
       ],
+      package_data={'tshistory_refinery': [
+          'refinery_static/*',
+          'templates/*'
+      ]},
       entry_points={
           'tshistory.subcommands': [
               'webstart=tshistory_refinery.cli:webstart',
@@ -30,5 +59,6 @@ setup(name='tshistory_refinery',
               'migrate-to-cache=tshistory_refinery.cli:migrate_to_cache',
               'setup-tasks=tshistory_refinery.cli:setup_tasks'
           ],
-      }
+      },
+      cmdclass={'build_ext': ElmBuild}
 )
