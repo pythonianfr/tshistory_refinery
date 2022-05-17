766eccf50c8cc18ae227f791ae962307e772ddd5
Aurelien Campeas <aurelien.campeas@pythonian.fr>
2022-05-17 18:49:17 +0200

    tasks/cache: separate the cached / uncached series
    At refresh time, if a series cache has been invalidated,
    put it at the end in order to not block other series.

--- tshistory_refinery/tasks.py
+++ tshistory_refinery/tasks.py
@@ -33,15 +33,37 @@ def refresh_formula_cache(task):
         tsh = tsa.tsh
         engine = tsa.engine
 
+        unames = set()
+        # put the uncached serie at the end
+        for name in names:
+            if not tsh.cache.exists(engine, name):
+                unames.add(name)
+
+        names = [
+            name for name in names
+            if name not in unames
+        ]
+
         cmp = helper.comparator(tsh, engine)
         names.sort(key=cmp_to_key(cmp))
 
+        unames = list(unames)
+        unames.sort(key=cmp_to_key(cmp))
+
+        # first batch (potentially just a refresh if not an initial run)
+        print('first batch')
         for name in names:
             print('refresh', name)
             with engine.begin() as cn:
                 if tsh.live_content_hash(cn, name) != tsh.content_hash(cn, name):
                     tsh.invalidate_cache(cn, name)
 
+            cache.refresh(engine, tsa, name, initial=task.input['initial'])
+
+        # second batch (potentially re-filling invalidated caches)
+        print('second batch')
+        for name in unames:
+            print('refresh', name)
             cache.refresh(engine, tsa, name, initial=task.input['initial'])
 
         cache.set_ready(engine, task.input['policy'])
