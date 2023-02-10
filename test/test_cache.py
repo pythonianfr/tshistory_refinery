17a1608346150c8d7cfe684cea94aad8d4bf257f
AC1195@engie.com <AC1195@engie.com>
2023-02-10 10:55:03 +0100

    tests/cache: add test on series with slice operator

--- test/test_cache.py
+++ test/test_cache.py
@@ -2092,3 +2092,58 @@ def test_cache_resample(tsa):
     series_no_cache = tsa.get(formula_name, nocache=True)
 
     print(tsa.tsh.cache.insertion_dates(tsa.engine, formula_name))
+
+
+def test_cache_slice(tsa):
+    days = list(range(1, 10))
+    # Define series
+    series_name = 'forecast'
+    series = pd.Series(
+        [day for day in days],
+        index=[pd.Timestamp(f'2023-01-{day:02d}') for day in days]
+    )
+    tsa.update(
+        series_name,
+        series,
+        'test_cache_slice',
+        insertion_date=pd.Timestamp('2023-01-01')
+    )
+    # Define formula
+    formula_name = 'sliced-forecast'
+    tsa.register_formula(
+        formula_name,
+        f'(slice (series "{series_name}") #:todate (today))',
+    )
+    # Define cache policy
+    policy_name = 'daily-cached'
+    tsa.new_cache_policy(
+        policy_name,
+        initial_revdate='(date "2023-01-05")',
+        look_before='(shifted now #:days -2)',
+        look_after='(shifted now #:days 10)',
+        revdate_rule='0 12 * * *',
+        schedule_rule='0 12 * * *',
+    )
+    tsa.set_cache_policy(
+        policy_name,
+        [formula_name]
+    )
+    cache.set_policy_ready(
+        tsa.engine,
+        policy_name,
+        True,
+        namespace=tsa.tsh.namespace
+    )
+    cache.refresh_series(
+        tsa.engine,
+        tsa,
+        formula_name,
+        final_revdate=pd.Timestamp('2023-1-2')
+    )
+
+    # Check: not good !
+    with pytest.raises(AssertionError):
+        pd.testing.assert_series_equal(
+            tsa.get(formula_name),
+            tsa.get(formula_name, nocache=True)
+        )
